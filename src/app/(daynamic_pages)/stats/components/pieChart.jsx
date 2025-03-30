d57cc926f6254b1bba04568e0259f5350e268b1f
Hussein Nahal <nahalhusssein1000@gmail.com>
2025-03-30 17:00:42 +0300

    version one

--- src/app/(daynamic_pages)/stats/components/pieChart.jsx
+++ src/app/(daynamic_pages)/stats/components/pieChart.jsx
@@ -14,7 +14,7 @@ export default function PieChartDemo() {
 
     useEffect(() => {
         const fetchData = async () => {
-            setLoader(true)
+            setLoader(true);
             try {
                 const token = Cookies.get("Walletly_Token");
                 const response = await axios.get(`https://walletlyapi.onrender.com/api/transaction/tranbyCat`, {
@@ -34,28 +34,26 @@ export default function PieChartDemo() {
                         {
                             data: chartValues,
                             backgroundColor: [
-                                 "#16a34a", "#3b82f6", "#a855f7","#f97316",
+                                "#16a34a", "#3b82f6", "#a855f7", "#f97316",
                                 "#9333ea", "#fb923c", "#d97706", "#16c7f6", "#0d9488",
-                                "#22c55e", "#e11d48", "#8b5cf6", "#06b6d4","#dc2626",
-                                "#d4d4d4", "#4ade80", "#f59e0b","#facc15", "#ec4899", "#6ee7b7"
+                                "#22c55e", "#e11d48", "#8b5cf6", "#06b6d4", "#dc2626",
+                                "#d4d4d4", "#4ade80", "#f59e0b", "#facc15", "#ec4899", "#6ee7b7"
                             ],
-                             
                             hoverBackgroundColor: [
-                                 "#15803d", "#2563eb", "#9333ea","#ea580c",
-                                 "#7e22ce", "#ea580c", "#ca8a04", "#0c4a6e", "#1e3a8a",
-                                 "#16a34a", "#9f1239", "#6d28d9", "#0891b2","#b91c1c",
-                                 "#9ca3af", "#64d2a6", "#eab308", "#eab308", "#d02670", "#10b981"
+                                "#15803d", "#2563eb", "#9333ea", "#ea580c",
+                                "#7e22ce", "#ea580c", "#ca8a04", "#0c4a6e", "#1e3a8a",
+                                "#16a34a", "#9f1239", "#6d28d9", "#0891b2", "#b91c1c",
+                                "#9ca3af", "#64d2a6", "#eab308", "#eab308", "#d02670", "#10b981"
                             ],
-                                                    }
+                        }
                     ]
                 };
 
                 setChartData(data);
             } catch (error) {
                 console.error("Error fetching transaction categories:", error);
-            }
-            finally{
-                setLoader(false)
+            } finally {
+                setLoader(false);
             }
         };
 
@@ -66,6 +64,14 @@ export default function PieChartDemo() {
                 legend: {
                     labels: {
                         usePointStyle: true
+                    }
+                },
+                tooltip: {
+                    callbacks: {
+                        label: function (tooltipItem) {
+                            let value = tooltipItem.raw; 
+                            return `$${value.toLocaleString()}`;
+                        }
                     }
                 }
             }
@@ -88,9 +94,9 @@ export default function PieChartDemo() {
                 </select>
             </div>
 
-            <div style={{display:"flex", justifyContent:"center", width:"100%"}}>
-                {loader? <Loader/>:
-                   <Chart type="pie" data={chartData} options={chartOptions} className={styles.pie} />
+            <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
+                {loader ? <Loader /> :
+                    <Chart type="pie" data={chartData} options={chartOptions} className={styles.pie} />
                 }
             </div>
         </div>
--- src/app/(daynamic_pages)/transactions/page.jsx
+++ src/app/(daynamic_pages)/transactions/page.jsx
@@ -246,32 +246,6 @@ export default function Transactions() {
         
 
 
-// pigination
-// import React, { useState } from "react";
-// import { Paginator } from 'primereact/paginator';
-
-// export default function BasicDemo() {
-//     const [first, setFirst] = useState(0);
-//     const [rows, setRows] = useState(10);
-
-//     const onPageChange = (event) => {
-//         setFirst(event.first);
-//         setRows(event.rows);
-//     };
-
-//     return (
-//         <div className="card">
-//             <Paginator first={first} rows={rows} totalRecords={120} rowsPerPageOptions={[10, 20, 30]} onPageChange={onPageChange} />
-//         </div>
-//     );
-// }
-        
-
-
-
-
-
-
 
 
 
